0440b6cfc92d0a3be30b0fe58a665032c3ee3d30
Julian <49489282+juy13@users.noreply.github.com>
2019-12-24 01:31:17 +0300

    выводит файлы из текущего каталога в следующем порядке:  Каталоги. Обычные файлы. Символьные ссылки. Символьные устройства. Блочные устройства.

---
+++ lab1_11.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+DIR=~/
+echo "Catalogs:"
+res_c=$(ls -l $DIR | grep ^d | tr -s ' ')
+echo "$res_c"
+echo "Files:"
+res_f=$(ls -l $DIR | grep "^-" | tr -s ' ')
+echo "$res_f"
+echo "Symb link: "
+res_sl=$(ls -l $DIR | grep ^l | tr -s ' ')
+echo "$res_sl"
+echo "Symb dev: "
+res_sd=$(ls -l $DIR | grep ^c | tr -s ' ')
+echo "$res_sd"
+echo "Block dev: "
+res_bd=$(ls -l $DIR | grep ^b | tr -s ' ')
+echo "$res_bd"
